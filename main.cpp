7212cc778e4caaa36802a06f94807cac1629041f
binamkayastha <binamkayastha@gmail.com>
2018-05-04 14:50:40 -0400

    Fixed phone resolution issues

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+//    qputenv("QT_SCALE_FACTOR", "0.75");
+//    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+//    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
 
